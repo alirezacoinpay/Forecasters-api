20c0e4e404e5fba339b12e3eb3f8f3e49f841c1c
AlirezaRoostaey <alirezaroostaey@gmail.com>
2026-07-19 00:26:02 +0330

    wip

--- app/Http/Middleware/AuthenticateFromCookie.php
+++ app/Http/Middleware/AuthenticateFromCookie.php
@@ -9,16 +9,15 @@ class AuthenticateFromCookie
 {
     public function handle($request, \Closure $next)
     {
-        Auth::setUser(User::find(2));
-//        $token = $request->cookie('auth_user');
-//
-//        if ($token) {
-//            $accessToken = PersonalAccessToken::findToken($token);
-//
-//            if ($accessToken) {
-//                Auth::setUser($accessToken->tokenable);
-//            }
-//        }
+        $token = $request->cookie('auth_user');
+
+        if ($token) {
+            $accessToken = PersonalAccessToken::findToken($token);
+
+            if ($accessToken) {
+                Auth::setUser($accessToken->tokenable);
+            }
+        }
 
         return $next($request);
     }
